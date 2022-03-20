76c51cb4f5740f7b873b135dc651859f8857d06e
mrlmarcela <mrl.marcelalima07@gmail.com>
2022-03-20 18:45:49 -0300

    update view

--- src/view/condominus/empty.view.php
+++ src/view/condominus/empty.view.php
@@ -1,13 +1,21 @@
-
 <main class="py-6 bg-surface-secondary">
     <div class="container-fluid">
-        <h3 class="display-4">Bem vindo!</h3>
-        <p class="lead">Dê o primeiro passo e cadastre seu primeiro condominio.</p>
-        <hr class="my-4">
-        <p class="lead">
-            <a class="btn btn-success btn-lg" href="<?php echo BASE_URL; ?>/condominus/create" role="button">Adicionar condomínios</a>
-        </p>
-        <div class="bs-docs-section">
+        <div class="border-bottom pt-6">
+            <div class="row align-items-center">
+                <div class="col-sm col-12">
+                    <h3 class="h1 ls-tight">Bem vindo!</h3>
+                    <p class="lead">Dê o primeiro passo e cadastre o seu primeiro condomínio.</p>
+                </div>
+                <div class="col-sm-auto col-12 mt-4 mt-sm-0">
+                    <div class="hstack gap-2 justify-content-sm-end">
+                        <a href="<?php echo BASE_URL; ?>/condominio/cadastro" class="btn btn-sm btn-primary">
+                            <span class="pe-2"><i class="bi bi-plus-square-dotted"></i> </span><span>Cadastrar condomínio</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bs-docs-section" style="margin-top:12%">
             <div class="row">
                 <div class="col-lg-12">
 
--- src/view/users/empty.view.php
+++ src/view/users/empty.view.php
@@ -1,18 +1,27 @@
 <main class="py-6 bg-surface-secondary">
     <div class="container-fluid">
-        <h3 class="display-4">Bem vindo!</h3>
-        <p class="lead">Dê o primeiro passo e matrícule seu primeiro condominio.</p>
-        <hr class="my-4">
-        <p class="lead">
-            <a class="btn btn-success btn-lg" href="<?php echo BASE_URL; ?>/users/create" role="button">Adicionar usuários</a>
-        </p>
-        <div class="bs-docs-section">
+        <div class="border-bottom pt-6">
+            <div class="row align-items-center">
+                <div class="col-sm col-12">
+                    <h3 class="h2 ls-tight">Ainda não temos nenhum condômino cadastrado.</h3>
+                    <p class="lead">Vamos lá, hora de cadastrar um condômino.</p>
+                </div>
+                <div class="col-sm-auto col-12 mt-4 mt-sm-0">
+                    <div class="hstack gap-2 justify-content-sm-end">
+                        <a href="<?php echo BASE_URL; ?>/users/cadastro" class="btn btn-sm btn-primary">
+                            <span class="pe-2"><i class="bi bi-plus-square-dotted"></i> </span><span>Cadastrar usuário</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bs-docs-section" style="margin-top:12%">
             <div class="row">
                 <div class="col-lg-12">
 
                     <div class="bs-component">
                         <div class="col-sm-12 text-center">
-                            <img src="<?php echo BASE_URL; ?>/assets/img/illustration/undraw_personal_training_0dqn.svg" width="400px" class="img-responsive" alt="">
+                            <img src="<?php echo BASE_URL; ?>/assets/img/illustration/undraw_coming_home_-52-ir.svg" style="height: 20rem;" class="img-responsive" alt="">
                         </div>
                     </div>
                 </div>
