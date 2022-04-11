db6f2eed27b95bd4bca748c5fd29d879c683ce35
Dionesio Guerra <dionesiojr@gmail.com>
2022-04-11 12:02:45 -0300

    configuraçao

--- src/controller/settings.controller.php
+++ src/controller/settings.controller.php
@@ -4,12 +4,37 @@ class Settings
 
     public function index()
     {
-        $this->conta();
+        $users_id = $_SESSION['user']['id'];
+        if (empty($users_id)) {
+            return redirect('/');
+        }
+
+        $data['users'] = User::show($users_id);
+        if (empty($data['users'])) {
+            return redirect('/');
+        }
+
+        _Application::applicationView('settings/conta', $data);
     }
 
-    public function conta()
+
+    public function destroy()
     {
-        $data = $_SESSION;
-        _Application::applicationView('settings/conta', $data);
+        $users_id = $_SESSION['user']['id'];
+
+        if (empty($users_id)) {
+            return false;
+        }
+
+
+        $result = User::destroy($users_id);
+
+        if (empty($result)) {
+            Alert::error("Falha ao tentar apagar a conta!");
+        } else {
+            Alert::success("Usuario apagado com sucesso!");
+        }
+
+        redirect('users');
     }
 }
